8c5b470ab1d1726b6c010d74eced98c5c3413009
Onlycanzzzz <guozisheng2022@126.com>
2026-08-27 19:43:53 +0800

    feat: 添加 LaTeX 数学公式支持 (MathJax)

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { // 如果你启用了即时加载，这个钩子可以保证公式重新渲染
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
